618e2bbc137acaf840ba5893d5359c04805257a4
DESKTOP-4N9SM0Q\rafa <raredown@gmail.com>
2016-10-09 13:44:22 +0200

    terminando el ejercicio

--- 20160927_Inicialito/src/java/es/albarregas/servlets/FormularioEnsServlet.java
+++ 20160927_Inicialito/src/java/es/albarregas/servlets/FormularioEnsServlet.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author rafa
  */
 @WebServlet(name = "FormularioGuay", urlPatterns = {"/FormularioGuay"})
-public class FormularioGuay extends HttpServlet {
+public class FormularioEnsServlet extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
